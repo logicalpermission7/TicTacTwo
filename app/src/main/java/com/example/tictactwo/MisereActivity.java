c6017749a4b3888e41726212333f15d8e3f7e7c7
ironman <elvisbueno1010@gmail.com>
2019-01-28 23:13:05 -0600

    images added and MisereActivity created for Gameplay

--- app/src/main/java/com/example/tictactwo/MisereActivity.java
+++ app/src/main/java/com/example/tictactwo/MisereActivity.java
@@ -1,4 +1,224 @@
 package com.example.tictactwo;
 
-public class MisereActivity {
+import android.app.Activity;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MisereActivity extends Activity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean player1Turn = true;
+
+    private int roundCount;
+
+    private int player1Points;
+    private int player2Points;
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+    private SoundPool soundPool;
+    private int sound1;
+    private int sound2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.misere_game);
+
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
+//-----------------------------------------------------------------------To play sounds from a sound pool start
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(6)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        }
+        sound1 = soundPool.load(this, R.raw.tic, 1);
+        sound2 = soundPool.load(this, R.raw.laugh, 1);
+
+
+//-----------------------------------------------------------------------To play sounds from a sound pool end
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+
+            }
+        }
+
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                soundPool.play(sound1, 1, 1, 0, 0, 1);
+                resetGame();
+
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (!((Button) v).getText().toString().equals("")) {
+            return;
+        }
+
+        if (player1Turn) {
+            ((Button) v).setText("x");
+            ((Button) v).setTextColor(Color.WHITE);
+
+
+        } else {
+            ((Button) v).setText("o");
+            ((Button) v).setTextColor(Color.WHITE);
+        }
+
+        roundCount++;
+
+        if (checkForWin()) {
+            if (player1Turn) {
+                player1Wins();
+            } else {
+                player2Wins();
+            }
+        } else if (roundCount == 9) {
+            draw();
+        } else {
+            player1Turn = !player1Turn;
+        }
+
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][0].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[0][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1])
+                && field[0][0].equals(field[2][2])
+                && !field[0][0].equals("")) {
+            return true;
+        }
+
+        if (field[0][2].equals(field[1][1])
+                && field[0][2].equals(field[2][0])
+                && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+
+
+    private void player1Wins() {
+        player2Points++;
+        Toast.makeText(this, "Player 1 loose!", Toast.LENGTH_LONG).show();
+        soundPool.play(sound2, 1, 1, 0, 0, 1);
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void player2Wins() {
+        player1Points++;
+        Toast.makeText(this, "Player 2 loose!", Toast.LENGTH_LONG).show();
+        soundPool.play(sound2, 1, 1, 0, 0, 1);
+        updatePointsText();
+        resetBoard();
+    }
+
+    private void draw() {
+        Toast.makeText(this, "Its a Draw!", Toast.LENGTH_LONG).show();
+        soundPool.play(sound1, 1, 1, 0, 0, 1);
+        resetBoard();
+    }
+
+    private void updatePointsText() {
+        textViewPlayer1.setText("Player 1 = " + player1Points);
+        textViewPlayer2.setText("Player 2 = " + player2Points);
+    }
+
+    private void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+            }
+        }
+
+        roundCount = 0;
+        player1Turn = true;
+    }
+
+    private void resetGame() {
+        player1Points = 0;
+        player2Points = 0;
+        updatePointsText();
+        resetBoard();
+
+
+    }
+
+    // for rotational purposes on phone. saves data when orientation is changed
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("roundCount", roundCount);
+        outState.putInt("playerPoints", player1Points);
+        outState.putInt("playerPoints", player2Points);
+        outState.putBoolean("player1Turn", player1Turn);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        player1Points = savedInstanceState.getInt("player1points");
+        player2Points = savedInstanceState.getInt("player2points");
+        player1Turn = savedInstanceState.getBoolean("player1Turn");
+
+    }
 }
+
